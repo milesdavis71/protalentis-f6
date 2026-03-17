6383092195450d6b23da37f343625cc399879e76
milesdavis71 <hevesiistvan71@hotmail.com>
2026-03-17 17:08:32 +0100

    chatgpt-vel telepítve a Lenovóra. Deprecation warningok javítva.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,7 +14,7 @@ import autoprefixer  from 'autoprefixer';
 import imagemin      from 'gulp-imagemin';
 
 
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const uncss = require('postcss-uncss');
 
@@ -99,10 +99,19 @@ function sassBuild() {
 
   return gulp.src('src/assets/scss/app.scss')
     .pipe($.sourcemaps.init())
-    .pipe(sass({
-      includePaths: PATHS.sass
+    .pipe(sass.sync({
+      includePaths: PATHS.sass,
+      quietDeps: true,
+      silenceDeprecations: [
+        'legacy-js-api',
+        'import',
+        'global-builtin',
+        'color-functions',
+        'slash-div',
+        'abs-percent'
+      ]
     })
-    .on('error', $.sass.logError))
+    .on('error', sass.logError))
     .pipe(postcss(postCssPlugins))
     .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie11' })))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
